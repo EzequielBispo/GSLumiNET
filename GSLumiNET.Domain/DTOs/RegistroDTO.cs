db466a35c18ef57b740bee8c38b80dfd6a170780
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-11-12 21:33:02 -0300

    Classe RegistroEntity e RegistroDTO implementadas.

---
+++ GSLumiNET.Domain/DTOs/RegistroDTO.cs
@@ -0,0 +1,17 @@
+﻿using GSLumiNET.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSLumiNET.Domain.DTOs
+{
+    public class RegistroDTO
+    {
+        public DateTime Data { get; set; }
+        public double IExterna { get; set; }
+        public double IInterna { get; set; }
+        public double ILampada { get; set; }
+    }
+}
---
+++ GSLumiNET.Domain/Entities/RegistroEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSLumiNET.Domain.Entities
+{
+    public class RegistroEntity
+    {
+        public DateTime Data { get; set; }
+        public double IExterna {  get; set; }
+        public double IInterna { get; set; }
+        public double ILampada { get; set; }
+    }
+}
