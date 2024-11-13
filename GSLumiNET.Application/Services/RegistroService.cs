3863314ac50534c0acc5f0dfa2dc63d29ef6b88c
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-11-12 21:46:09 -0300

    Implementação da classe RegistroService sem métodos.

---
+++ GSLumiNET.Application/Services/RegistroService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GSLumiNET.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace GSLumiNET.Application.Services
+{
+    public class RegistroService : IRegistroService
+    {
+        private readonly IRegistroRepository _repository;
+
+        public RegistroService(IRegistroRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
