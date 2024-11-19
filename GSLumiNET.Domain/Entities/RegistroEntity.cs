64a5068997cbf0ab94806ba3dcf237e53bcbfbf5
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-11-18 22:15:16 -0300

    Implementação do Id na classe de entidade.

--- GSLumiNET.Domain/Entities/RegistroEntity.cs
+++ GSLumiNET.Domain/Entities/RegistroEntity.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace GSLumiNET.Domain.Entities
 {
     public class RegistroEntity
     {
+        [Key]
+        public int Id { get; set; }
         public DateTime Data { get; set; }
         public double IExterna {  get; set; }
         public double IInterna { get; set; }
