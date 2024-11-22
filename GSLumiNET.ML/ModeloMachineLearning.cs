7b5e494cfa0817995b90909804578337810c9249
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-11-22 20:18:04 -0300

    Implementação do modelo de ML.

---
+++ GSLumiNET.ML/ModeloMachineLearning.cs
@@ -0,0 +1,40 @@
+﻿namespace GSLumiNET.ML;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ModeloMachineLearning
+{
+    private static string _dadosTreinamento = @"dados.csv"; 
+    public static void TreinarModelo()
+    {
+        var mlContext = new MLContext();
+
+        var dadosTreinamento = mlContext.Data.LoadFromTextFile<DadosEntrada>(_dadosTreinamento, separatorChar: ',', hasHeader: true);
+
+        var pipeline = mlContext.Regression.Trainers.Sdca(labelColumnName: "ILampada", maximumNumberOfIterations: 100);
+
+        var modelo = pipeline.Fit(dadosTreinamento);
+
+        mlContext.Model.Save(modelo, dadosTreinamento.Schema, "modelo.zip");
+        Console.WriteLine("Modelo treinado e salvo.");
+    }
+
+    public static void RealizarPredicao()
+    {
+        var mlContext = new MLContext();
+        var modelo = mlContext.Model.Load("modelo.zip", out var modelInputSchema);
+
+        var dadosParaPredicao = new DadosEntrada()
+        {
+            IExterna = 2.5f,
+            IInterna = 3.0f
+        };
+
+        var predicaoFunc = mlContext.Model.CreatePredictionFunction<DadosEntrada, DadosSaida>(modelo);
+
+        var resultado = predicaoFunc.Predict(dadosParaPredicao);
+        Console.WriteLine($"Predição para ILampada: {resultado.ILampada}");
+    }
+}
